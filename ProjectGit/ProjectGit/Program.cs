97fd500de32c47e933fe35e14d57e03bd9e2d8be
FedorovMisha <60353834+FedorovMisha@users.noreply.github.com>
2020-02-19 13:59:33 +0300

    asd

--- ProjectGit/ProjectGit/Program.cs
+++ ProjectGit/ProjectGit/Program.cs
@@ -11,6 +11,7 @@ namespace ProjectGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hellow World");
+            Console.WriteLine("Good Job");
         }
     }
 }
